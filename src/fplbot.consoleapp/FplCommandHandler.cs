5fe9f4408c1fdd9fa56c2ef15cd87a79ad65fc1d
Korsneso <johnkors@gmail.com>
2019-12-06 11:13:01 +0100

    Adds Slackbot basic

---
+++ src/fplbot.consoleapp/FplCommandHandler.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Slackbot.Net.Handlers;
+using Slackbot.Net.Publishers;
+using SlackConnector.Models;
+
+namespace fplbot.consoleapp
+{
+    public class FplCommandHandler : IHandleMessages
+    {
+        private readonly IEnumerable<IPublisher> _publishers;
+
+        public FplCommandHandler(IEnumerable<IPublisher> publishers)
+        {
+            _publishers = publishers;
+        }
+        public async Task<HandleResponse> Handle(SlackMessage message)
+        {
+            foreach (var p in _publishers)
+            {
+                await p.Publish(new Notification
+                {
+                    BotName = "fpl",
+                    Channel = "#testss",
+                    Msg = "fpl pong",
+                    IconEmoji = ":santa:"
+                });
+            }
+            return new HandleResponse("OK");
+        }
+
+        public bool ShouldHandle(SlackMessage message)
+        {
+            return message.MentionsBot && message.Text.Contains("fpl");
+        }
+    }
+}
---
+++ src/fplbot.consoleapp/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Slackbot.Net.Hosting;
+using Slackbot.Net.Publishers;
+using Slackbot.Net.Publishers.Slack;
+
+namespace fplbot.consoleapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSlackbot(o =>
+                        {
+                            o.Slackbot_SlackApiKey_SlackApp = Environment.GetEnvironmentVariable("Slackbot_SlackApiKey_SlackApp");
+                        })
+
+                        .AddPublisher<SlackPublisher>()
+                        .AddPublisher<LoggerPublisher>()
+                        .AddHandler<FplCommandHandler>();
+
+                });
+    }
+}
