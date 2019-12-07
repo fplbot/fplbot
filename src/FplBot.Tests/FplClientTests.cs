137708100cb6c28661dc268c24c0593ecbfa3c34
Korsneso <johnkors@gmail.com>
2019-12-07 13:31:40 +0100

    Fix dockerfiles

--- src/FplBot.Tests/FplClientTests.cs
+++ src/FplBot.Tests/FplClientTests.cs
@@ -2,11 +2,19 @@ using System;
 using System.Threading.Tasks;
 using FplBot.ConsoleApps.Clients;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace FplBot.Tests
 {
     public class FplClientTests
     {
+        private readonly ITestOutputHelper _logger;
+
+        public FplClientTests(ITestOutputHelper logger)
+        {
+            _logger = logger;
+        }
+        
         [Fact]
         public async Task GetStandings()
         {
