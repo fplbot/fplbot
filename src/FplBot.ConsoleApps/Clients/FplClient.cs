5a54783dfa33456e1dfc0f4b8dfdbb00fa9ead98
Korsneso <johnkors@gmail.com>
2019-12-11 11:53:45 +0100

    Unused usings

--- src/FplBot.ConsoleApps/Clients/FplClient.cs
+++ src/FplBot.ConsoleApps/Clients/FplClient.cs
@@ -1,12 +1,9 @@
 ﻿using FplBot.ConsoleApps.Models;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Flurl.Util;
 
 namespace FplBot.ConsoleApps.Clients
 {
