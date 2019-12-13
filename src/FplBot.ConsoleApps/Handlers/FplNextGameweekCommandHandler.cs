a4fe49e293fc4fbc11ffe16b5d2e8b4633f10c89
Korsneso <johnkors@gmail.com>
2019-12-13 09:44:43 +0100

    Cleanup formatting nextgw response

--- src/FplBot.ConsoleApps/Handlers/FplNextGameweekCommandHandler.cs
+++ src/FplBot.ConsoleApps/Handlers/FplNextGameweekCommandHandler.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Fpl.Client.Abstractions;
+using Fpl.Client.Models;
 using Slackbot.Net.Workers.Handlers;
 using Slackbot.Net.Workers.Publishers;
 using SlackConnector.Models;
@@ -32,18 +34,12 @@ namespace FplBot.ConsoleApps.Handlers
         public async Task<HandleResponse> Handle(SlackMessage message)
         {
             var gameweeks = await _gameweekClient.GetGameweeks();
-            var currentGw = gameweeks.SingleOrDefault(x => x.IsCurrent)?.Id;
-            var fixtures = await _fixtureClient.GetFixturesByGameweek(currentGw.Value+1);
+            var nextGw = gameweeks.First(gw => gw.IsNext);
+            var fixtures = await _fixtureClient.GetFixturesByGameweek(nextGw.Id);
             var teams = await _teamsclient.GetAllTeams();
 
-            var textToSend = "";
-            foreach (var fixture in fixtures)
-            {
-                var homeTeam = teams.First(t => t.Id == fixture.HomeTeamId);
-                var awayTeam = teams.First(t => t.Id == fixture.AwayTeamId);
-                textToSend += $"\n * {homeTeam.ShortName}-{awayTeam.ShortName}";
-            }
-            
+            var textToSend = TextToSend(nextGw, fixtures, teams);
+
             foreach (var p in _publishers)
             {
                 await p.Publish(new Notification
@@ -54,6 +50,34 @@ namespace FplBot.ConsoleApps.Handlers
             }
 
             return new HandleResponse(textToSend);
+        }
+
+        private static string TextToSend(Gameweek gameweek, ICollection<Fixture> fixtures, ICollection<Team> teams)
+        {
+            var textToSend = $":information_source: <https://fantasy.premierleague.com/fixtures/{gameweek.Id}|{gameweek.Name.ToUpper()}>\nDeadline: {ConvertToNorwegianTimeZone(gameweek.Deadline)}";
+            foreach (var fixture in fixtures)
+            {
+                var homeTeam = teams.First(t => t.Id == fixture.HomeTeamId);
+                var awayTeam = teams.First(t => t.Id == fixture.AwayTeamId);
+                textToSend += $"\n• {homeTeam.ShortName}-{awayTeam.ShortName}";
+            }
+
+            return textToSend;
+        }
+        
+        private static string ConvertToNorwegianTimeZone(DateTime dateToConvertTime)
+        {
+            var timeZoneId = @"Europe/Oslo";
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                timeZoneId = "Central European Standard Time";
+            }
+
+            var norwegianZoneId = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+
+            var date = TimeZoneInfo.ConvertTime(dateToConvertTime, norwegianZoneId);
+            return date.ToString("yyyy-MM-hh HH:mm");
         }
 
         public bool ShouldHandle(SlackMessage message)
