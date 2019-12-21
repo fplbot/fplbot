ca0ee5c582ade77e0f46f8d1884e3655281ce85f
John Korsnes <johnkors@gmail.com>
2019-12-21 13:12:50 +0100

    Simplifies Heroku.Dockerfile (#18)

---
+++ buildherokuimage.sh
@@ -0,0 +1,1 @@
+docker build --rm -t fplbot:heroku -f Heroku.Dockerfile .
---
+++ runherokuimage.sh
@@ -0,0 +1,10 @@
+ #!/usr/bin/env bash
+# Runs Fplbot in docker
+
+ docker run --rm \
+ -e fpl__login=$fpl__login \
+ -e fpl__password=$fpl__password \
+ -e Slackbot_SlackApiKey_SlackApp=$Slackbot_SlackApiKey_SlackApp \
+ -e Slackbot_SlackApiKey_BotUser=$Slackbot_SlackApiKey_BotUser \
+ -e leagueId=89903 \
+ fplbot:heroku
