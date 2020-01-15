00beab48e96cc3d61e15a578d510a56f41fc8c32
John Korsnes <johnkors@gmail.com>
2020-01-15 22:48:10 +0100

    Adds near deadline notification (#29)
    * Adds near deadline notification
    
    * Adds more tests and fixes bugs
    
    * Adds @channel to near deadline notification

---
+++ src/Slackbot.Net.Extensions.FplBot/Helpers/DateTimeUtils.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Slackbot.Net.Extensions.FplBot.Helpers
+{
+    internal class DateTimeUtils
+    {
+        public bool IsWithinMinutesToDate(int minutes, DateTime dateUtc)
+        {
+            var threshold = dateUtc.Subtract(TimeSpan.FromMinutes(minutes));
+            return NowUtc >= threshold && NowUtc < dateUtc && NowUtc.Minute == dateUtc.Minute;
+        }
+
+        public DateTime NowUtc { get; set; } = DateTime.UtcNow;
+    }
+}
