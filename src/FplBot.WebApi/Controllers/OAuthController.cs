06171499397b769ca4996ea325e0368aed2f8efd
John Korsnes <johnkors@gmail.com>
2020-07-01 21:10:52 +0200

    Simple classic leagueId validation on install

--- src/FplBot.WebApi/Controllers/OAuthController.cs
+++ src/FplBot.WebApi/Controllers/OAuthController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Net;
 using System.Threading.Tasks;
-using FplBot.WebApi.Data;
-using Microsoft.AspNetCore.Http;
+using Fpl.Client.Abstractions;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
@@ -24,39 +22,36 @@ namespace FplBot.WebApi.Controllers
         private readonly ISlackOAuthAccessClient _oAuthAccessClient;
         private readonly ISlackTeamRepository _slackTeamRepository;
         private readonly IFetchFplbotSetup _setupFetcher;
+        private readonly ILeagueClient _leagueClient;
         private readonly IOptions<DistributedSlackAppOptions> _options;
 
-        public OAuthController(ILogger<OAuthController> logger, ISlackOAuthAccessClient oAuthAccessClient, ISlackTeamRepository slackTeamRepository, IFetchFplbotSetup setupFetcher, IOptions<DistributedSlackAppOptions> options)
+        public OAuthController(ILogger<OAuthController> logger, ISlackOAuthAccessClient oAuthAccessClient, ISlackTeamRepository slackTeamRepository, IFetchFplbotSetup setupFetcher, ILeagueClient leagueClient, IOptions<DistributedSlackAppOptions> options)
         {
             _logger = logger;
             _oAuthAccessClient = oAuthAccessClient;
             _slackTeamRepository = slackTeamRepository;
             _setupFetcher = setupFetcher;
+            _leagueClient = leagueClient;
             _options = options;
         }
 
-        [HttpGet("workspaces")]
-        public async Task<IActionResult> Debug()
-        {
-            var allWorkspaces = await _setupFetcher.GetAllFplBotSetup();
-            return Ok(allWorkspaces);
-        }
-
         [HttpGet("install")]
-        public IActionResult Install(string channel, string leagueId)
+        public async Task<IActionResult> Install(string channel, string leagueId)
         {
             _logger.LogInformation($"Installing using channel {channel} and league {leagueId}!");
+            try
+            {
+                await _leagueClient.GetClassicLeague(int.Parse(leagueId));
+            }
+            catch (Exception)
+            {
+                var msg = $"Could not find FPL league with id `{leagueId}. Only classic leagues are currently supported (not draft leagues)`";
+                return Redirect($"/error?msg={msg}");
+            }
             var urlencodedState = WebUtility.UrlEncode($"{channel},{leagueId}");
             var original = new Uri(HttpContext.Request.GetDisplayUrl());
             var redirect_uri = new Uri(original, "/oauth/authorize");
             return Redirect($"https://slack.com/oauth/v2/authorize?&user_scope=&scope=app_mentions:read,chat:write,chat:write.customize,chat:write.public,users.profile:read,users:read,users:read.email&client_id={_options.Value.CLIENT_ID}&state={urlencodedState}&redirect_uri={redirect_uri}");
-        }
-
-        [HttpGet("uninstall")]
-        public async Task<IActionResult> Uninstall(string teamId)
-        {
-            await _slackTeamRepository.DeleteByTeamId(teamId);
-            return Ok("Ok");
         }
 
         [HttpGet("authorize")]
