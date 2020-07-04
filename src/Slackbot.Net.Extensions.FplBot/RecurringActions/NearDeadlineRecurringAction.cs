a5a82b5900c08fd9f67dde63052b00705a3ab644
John Korsnes <johnkors@gmail.com>
2020-07-04 11:43:12 +0200

    Error handling in neardeadline-notification

--- src/Slackbot.Net.Extensions.FplBot/RecurringActions/NearDeadlineRecurringAction.cs
+++ src/Slackbot.Net.Extensions.FplBot/RecurringActions/NearDeadlineRecurringAction.cs
@@ -2,12 +2,10 @@ using System;
 using Fpl.Client.Abstractions;
 using Microsoft.Extensions.Logging;
 using Slackbot.Net.Abstractions.Handlers;
-using Slackbot.Net.Abstractions.Hosting;
 using Slackbot.Net.Extensions.FplBot.Helpers;
-using Slackbot.Net.SlackClients.Http;
 using System.Linq;
 using System.Threading.Tasks;
-using Slackbot.Net.Extensions.FplBot.Abstractions;
+using Slackbot.Net.Extensions.FplBot.GameweekLifecycle;
 
 namespace Slackbot.Net.Extensions.FplBot.RecurringActions
 {
@@ -15,26 +13,21 @@ namespace Slackbot.Net.Extensions.FplBot.RecurringActions
     {
         private readonly IGameweekClient _gwClient;
         private readonly DateTimeUtils _dateTimeUtils;
-        private readonly ISlackClientBuilder _slackClientBuilder;
+        private readonly ISlackWorkSpacePublisher _workspacePublisher;
         private readonly ILogger<NearDeadlineRecurringAction> _logger;
         private readonly int _minutesBeforeDeadline;
-        private readonly ITokenStore _tokenStore;
-        private readonly IFetchFplbotSetup _teamRepo;
+  
 
         public NearDeadlineRecurringAction(
             IGameweekClient gwClient, 
             DateTimeUtils dateTimeUtils, 
-            ISlackClientBuilder slackClientBuilder, 
-            ILogger<NearDeadlineRecurringAction> logger, 
-            ITokenStore tokenStore, 
-            IFetchFplbotSetup teamRepo)
+            ISlackWorkSpacePublisher workspacePublisher,
+            ILogger<NearDeadlineRecurringAction> logger)
         {
             _gwClient = gwClient;
             _dateTimeUtils = dateTimeUtils;
-            _slackClientBuilder = slackClientBuilder;
+            _workspacePublisher = workspacePublisher;
             _logger = logger;
-            _tokenStore = tokenStore;
-            _teamRepo = teamRepo;
             _minutesBeforeDeadline = 60;
         }
 
@@ -54,7 +47,7 @@ namespace Slackbot.Net.Extensions.FplBot.RecurringActions
             if(_dateTimeUtils.IsWithinMinutesToDate(_minutesBeforeDeadline, current.Deadline))
             {
                 _logger.LogDebug($"Notifying, since <{_minutesBeforeDeadline} minutes to current (gw{current.Id}) deadline");
-                await Publish($"<!channel> Gameweek {current.Id} deadline in 1 hour!");
+                await _workspacePublisher.PublishToAllWorkspaces($"<!channel> Gameweek {current.Id} deadline in 1 hour!");
                 return;
             }
             
@@ -69,7 +62,7 @@ namespace Slackbot.Net.Extensions.FplBot.RecurringActions
             if(_dateTimeUtils.IsWithinMinutesToDate(_minutesBeforeDeadline, next.Deadline))
             {
                 _logger.LogDebug($"Notifying, since <{_minutesBeforeDeadline} minutes to next (gw{next.Id}) deadline");
-                await Publish($"<!channel> Gameweek {next.Id} deadline in 1 hour!");
+                await _workspacePublisher.PublishToAllWorkspaces($"<!channel> Gameweek {next.Id} deadline in 1 hour!");
             }
             
             else
@@ -81,22 +74,6 @@ namespace Slackbot.Net.Extensions.FplBot.RecurringActions
                                  $"No notification.");
             }
         }
-
-        protected async Task Publish(string msg)
-        {
-            var tokens = await _tokenStore.GetTokens();
-            foreach (var token in tokens)
-            {
-                var setup = await _teamRepo.GetSetupByToken(token);
-
-                var slackClient = _slackClientBuilder.Build(token);
-                var res = await slackClient.ChatPostMessage(setup.Channel, msg);
-
-                if (!res.Ok)
-                    _logger.LogError($"Could not post to {setup.Channel}", res.Error);
-            }
-        }
-
         public string Cron => Constants.CronPatterns.EveryMinuteAt20Seconds;
     }
 }
