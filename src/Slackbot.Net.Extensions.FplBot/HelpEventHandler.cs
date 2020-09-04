8938cf28e8332093f3be8ea6a263cfc463a1cd3b
John Korsnes <johnkors@gmail.com>
2020-09-04 14:54:33 +0200

    Showcase hack for handling unknown events
    Unknown in that Slackbot.NET does not have a model for it

--- src/Slackbot.Net.Extensions.FplBot/HelpEventHandler.cs
+++ src/Slackbot.Net.Extensions.FplBot/HelpEventHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Slackbot.Net.Dynamic;
 using Slackbot.Net.Endpoints.Abstractions;
 using Slackbot.Net.Endpoints.Models;
@@ -12,22 +13,36 @@ namespace Slackbot.Net.Extensions.FplBot
     {
         private readonly IEnumerable<IHandleEvent> _handlers;
         private readonly ISlackClientService _slackClientService;
+        private readonly ILogger<HelpEventHandler> _logger;
 
-        public HelpEventHandler(IEnumerable<IHandleEvent> allHandlers, ISlackClientService slackClientService)
+        public HelpEventHandler(IEnumerable<IHandleEvent> allHandlers, ISlackClientService slackClientService, ILogger<HelpEventHandler> logger)
         {
             _handlers = allHandlers;
             _slackClientService = slackClientService;
+            _logger = logger;
         }
 
         public async Task Handle(EventMetaData eventMetadata, SlackEvent @event)
         {
-            var text = _handlers.Where(h => !(h is FplBotJoinedChannelHandler)).Select(handler => handler.GetHelpDescription())
-                .Aggregate("*HALP:*", (current, helpDescription) => current + $"\n• `{helpDescription.HandlerTrigger}` : _{helpDescription.Description}_");
-            
-            var appMention = (AppMentionEvent) @event;
+            switch (@event)
+            {
+                case UnknownSlackEvent unknown:
+                    // do stuff with new events not in Slackbot.Net, i.e. : `app_home_opened` 
+                    _logger.LogInformation(unknown.RawJson);
+                    break;
+                case AppMentionEvent appMention:
+                {
+                    var text = _handlers.Where(h => !(h is FplBotJoinedChannelHandler)).Select(handler => handler.GetHelpDescription())
+                        .Aggregate("*HALP:*", (current, helpDescription) => current + $"\n• `{helpDescription.HandlerTrigger}` : _{helpDescription.Description}_");
 
-            var slackClient = await _slackClientService.CreateClient(eventMetadata.Team_Id);
-            await slackClient.ChatPostMessage(appMention.Channel, text);        
+                    var slackClient = await _slackClientService.CreateClient(eventMetadata.Team_Id);
+                    await slackClient.ChatPostMessage(appMention.Channel, text);
+                    break;
+                }
+                default:
+                    _logger.LogWarning($"Received unhandled event: {@event.Type}");
+                    break;
+            }
         }
 
         public bool ShouldHandle(SlackEvent @event)
