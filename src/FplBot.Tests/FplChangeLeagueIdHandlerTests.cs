43b0813fb58bae0d2572699a81fdc67fac15822d
John Korsnes <johnkors@gmail.com>
2020-09-05 13:04:45 +0200

    Adds slack notifications to updateleagueid cmnd

--- src/FplBot.Tests/FplChangeLeagueIdHandlerTests.cs
+++ src/FplBot.Tests/FplChangeLeagueIdHandlerTests.cs
@@ -10,10 +10,12 @@ namespace FplBot.Tests
 {
     public class FplChangeLeagueIdHandlerTests
     {
+        private readonly ITestOutputHelper _logger;
         private readonly IHandleEvent _client;
 
         public FplChangeLeagueIdHandlerTests(ITestOutputHelper logger)
         {
+            _logger = logger;
             _client = Factory.GetHandler<FplChangeLeagueIdHandler>(logger);
         }
         
@@ -22,7 +24,35 @@ namespace FplBot.Tests
         {
             var dummy = Factory.CreateDummyEvent("updateleagueid 1337");
             var response = await _client.Handle(dummy.meta, dummy.@event);
+            _logger.WriteLine(response.Response);
             Assert.Contains("Thanks! You're now following", response.Response, StringComparison.InvariantCultureIgnoreCase);
+        }
+        
+        [Fact]
+        public async Task ChangeToInvalidLeagueIdShouldNotUpdate()
+        {
+            var dummy = Factory.CreateDummyEvent("updateleagueid abc");
+            var response = await _client.Handle(dummy.meta, dummy.@event);
+            _logger.WriteLine(response.Response);
+            Assert.Contains("Could not update league to id 'abc'. Make sure it's a valid number.", response.Response, StringComparison.InvariantCultureIgnoreCase);
+        }
+        
+        [Fact]
+        public async Task ChangeToNotFoundLeagueIdShouldNotUpdate()
+        {
+            var dummy = Factory.CreateDummyEvent("updateleagueid 0");
+            var response = await _client.Handle(dummy.meta, dummy.@event);
+            _logger.WriteLine(response.Response);
+            Assert.Contains("Could not find league 0 :/ Could you find it at https://fantasy.premierleague.com/leagues/0/standings/c ?", response.Response, StringComparison.InvariantCultureIgnoreCase);
+        }
+        
+        [Fact]
+        public async Task ChangeToMissingArgsProvidesHelpText()
+        {
+            var dummy = Factory.CreateDummyEvent("updateleagueid");
+            var response = await _client.Handle(dummy.meta, dummy.@event);
+            _logger.WriteLine(response.Response);
+            Assert.Contains("No leagueId provided. Usage: `@fplbot updateleagueid 123`", response.Response, StringComparison.InvariantCultureIgnoreCase);
         }
     }
 }
--- src/Slackbot.Net.Extensions.FplBot/Handlers/FplChangeLeagueIdHandler.cs
+++ src/Slackbot.Net.Extensions.FplBot/Handlers/FplChangeLeagueIdHandler.cs
@@ -1,9 +1,13 @@
-﻿using Fpl.Client.Abstractions;
+﻿using System;
+using System.Net.Http;
+using Fpl.Client.Abstractions;
 using Slackbot.Net.Endpoints.Abstractions;
 using Slackbot.Net.Endpoints.Models;
 using Slackbot.Net.Extensions.FplBot.Abstractions;
 using Slackbot.Net.Extensions.FplBot.Helpers;
 using System.Threading.Tasks;
+using Fpl.Client.Models;
+using Microsoft.Extensions.Logging;
 
 namespace Slackbot.Net.Extensions.FplBot.Handlers
 {
@@ -11,11 +15,15 @@ namespace Slackbot.Net.Extensions.FplBot.Handlers
     {
         private readonly ISlackTeamRepository _slackTeamRepository;
         private readonly ILeagueClient _leagueClient;
+        private readonly ISlackWorkSpacePublisher _publisher;
+        private readonly ILogger<FplChangeLeagueIdHandler> _logger;
 
-        public FplChangeLeagueIdHandler(ISlackTeamRepository slackTeamRepository, ILeagueClient leagueClient)
+        public FplChangeLeagueIdHandler(ISlackTeamRepository slackTeamRepository, ILeagueClient leagueClient, ISlackWorkSpacePublisher publisher, ILogger<FplChangeLeagueIdHandler> logger)
         {
             _slackTeamRepository = slackTeamRepository;
             _leagueClient = leagueClient;
+            _publisher = publisher;
+            _logger = logger;
         }
 
         public async Task<EventHandledResponse> Handle(EventMetaData eventMetadata, SlackEvent slackEvent)
@@ -23,11 +31,43 @@ namespace Slackbot.Net.Extensions.FplBot.Handlers
             var message = slackEvent as AppMentionEvent;
 
             var newLeagueId = ParseLeagueId(message);
-            await _slackTeamRepository.UpdateLeagueId(eventMetadata.Team_Id, long.Parse(newLeagueId));
 
-            var league = await _leagueClient.GetClassicLeague(int.Parse(newLeagueId));
+            if (string.IsNullOrEmpty(newLeagueId))
+            {
+                var help = $"No leagueId provided. Usage: `@fplbot updateleagueid 123`";
+                await _publisher.PublishToWorkspace(eventMetadata.Team_Id, message.Channel, help);
+                return new EventHandledResponse(help);
+            }
 
-            return new EventHandledResponse($"Thanks! You're now following {league.Properties.Name}.");
+            var couldParse = int.TryParse(newLeagueId, out var theLeagueId);
+            
+            if (!couldParse)
+            {
+                var res = $"Could not update league to id '{newLeagueId}'. Make sure it's a valid number.";
+                await _publisher.PublishToWorkspace(eventMetadata.Team_Id, message.Channel, res);
+                return new EventHandledResponse(res);
+            }
+
+            var failure = $"Could not find league {newLeagueId} :/ Could you find it at https://fantasy.premierleague.com/leagues/{newLeagueId}/standings/c ?";
+            try
+            {
+                var league = await _leagueClient.GetClassicLeague(theLeagueId);
+                
+                if (league?.Properties != null)
+                {
+                    await _slackTeamRepository.UpdateLeagueId(eventMetadata.Team_Id, theLeagueId);
+                    var success = $"Thanks! You're now following the '{league.Properties.Name}' league (leagueId: {theLeagueId})";
+                    await _publisher.PublishToWorkspace(eventMetadata.Team_Id, message.Channel, success);
+                    return new EventHandledResponse(success);    
+                }
+                await _publisher.PublishToWorkspace(eventMetadata.Team_Id, message.Channel, failure);
+                return new EventHandledResponse(failure);
+            }
+            catch (HttpRequestException e)
+            {
+                _logger.LogError(e.Message, e);
+                return new EventHandledResponse(failure);
+            }
         }
 
         private static string ParseLeagueId(AppMentionEvent message)
