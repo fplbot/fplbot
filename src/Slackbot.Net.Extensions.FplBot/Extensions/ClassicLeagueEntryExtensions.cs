7d6fbed6e587e01d8cd1f30b9b04b1314d6175d3
John Korsnes <johnkors@gmail.com>
2020-09-12 13:35:23 +0200

    Use new_entries when no standings are available (#92)

--- src/Slackbot.Net.Extensions.FplBot/Extensions/ClassicLeagueEntryExtensions.cs
+++ src/Slackbot.Net.Extensions.FplBot/Extensions/ClassicLeagueEntryExtensions.cs
@@ -1,9 +1,15 @@
 ﻿using Fpl.Client.Models;
+using Slackbot.Net.Extensions.FplBot.Helpers;
 
 namespace Slackbot.Net.Extensions.FplBot.Extensions
 {
     public static class ClassicLeagueEntryExtensions
     {
+        public static string GetEntryLink(this GenericEntry entry, int? gameweek)
+        {
+            return $"<https://fantasy.premierleague.com/entry/{entry.Entry}/event/{gameweek}|{entry.EntryName}>";
+        }
+        
         public static string GetEntryLink(this ClassicLeagueEntry entry, int? gameweek)
         {
             return $"<https://fantasy.premierleague.com/entry/{entry.Entry}/event/{gameweek}|{entry.EntryName}>";
