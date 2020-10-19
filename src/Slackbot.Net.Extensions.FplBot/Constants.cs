842088b21b9521a0d551937624fdf4dd748652e6
Erlend Hamberg <erlend.hamberg@blank.no>
2020-10-19 08:35:18 +0200

    Fix spelling of “jeez”

--- src/Slackbot.Net.Extensions.FplBot/Constants.cs
+++ src/Slackbot.Net.Extensions.FplBot/Constants.cs
@@ -120,7 +120,7 @@
         {
             public static readonly string[] TransferredGoalScorerOutTaunts =
             {
-                "Ah jiiz, you transferred him out, {0} :joy:",
+                "Ah jeez, you transferred him out, {0} :joy:",
                 "You just had to knee jerk him out, didn't you, {0}?",
                 "Didn't you have that guy last week, {0}?",
                 "Goddammit, really? You couldn't hold on to him just one more gameweek, {0}?"
