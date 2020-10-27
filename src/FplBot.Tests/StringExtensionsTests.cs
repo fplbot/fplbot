453948939e61c6dea1319997087b10b1a6728c88
Lars Skjelbek <lars.skjelbek@gmail.com>
2020-10-27 21:32:33 +0100

    Add a couple of tests for the eventsubscription parser

---
+++ src/FplBot.Tests/StringExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Slackbot.Net.Extensions.FplBot.Abstractions;
+using Slackbot.Net.Extensions.FplBot.Extensions;
+using Xunit;
+
+namespace FplBot.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("fixtureassists, bøler", new []{EventSubscription.FixtureAssists}, new []{"bøler"})]
+        [InlineData("fixtureassists, fixturegoals", new []{EventSubscription.FixtureAssists, EventSubscription.FixtureGoals}, null)]
+        [InlineData("fixtureassists, fixturegoals, all, cardss", new []{EventSubscription.FixtureAssists, EventSubscription.FixtureGoals, EventSubscription.All}, new []{"cardss"})]
+        public void ParseSubscriptionString_ShouldWork(string input, EventSubscription[] expected, string[] unableToParse) 
+        {
+            // Act
+            var result = input.ParseSubscriptionString(",", out var errors);
+
+            // Assert
+            Assert.True(result.All(expected.Contains));
+            Assert.True(unableToParse == null && !errors.Any() || errors.All(unableToParse.Contains));
+        }
+    }
+}
