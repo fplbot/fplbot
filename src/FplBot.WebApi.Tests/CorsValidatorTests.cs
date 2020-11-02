dd47f94d2e6ff9da27163e8a9a56f1c7a70d148e
John Korsnes <johnkors@gmail.com>
2020-11-02 15:51:00 +0100

    Adds CORS support for Heroku Review apps
    Dynamic subdomains is created by Heroku

---
+++ src/FplBot.WebApi.Tests/CorsValidatorTests.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace FplBot.WebApi.Tests
+{
+    public class CorsValidatorTests
+    {
+        [Theory]
+        [InlineData("http://localhost:3000", true)]
+        [InlineData("https://fplbot-frontend.herokuapp.com", true)]
+        [InlineData("https://www.fplbot.app", true)]
+        [InlineData("https://fplbotfrontend-pr-4.herokuapp.com", true)]
+        [InlineData("https://fplbotfrontend-pr-12.herokuapp.com", true)]
+        [InlineData("https://someotherdomain-pr-12.herokuapp.com", false)]
+        public void ValidatesOrigins(string origin, bool expectedResult)
+        {
+            var isValid = CorsOriginValidator.ValidateOrigin(origin);
+            Assert.Equal(expectedResult,isValid);
+        }
+    }
+}
