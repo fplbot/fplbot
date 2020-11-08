a78d2d2052666d43fc39274a467874d94d8c3c71
John Korsnes <johnkors@gmail.com>
2020-11-08 12:51:32 +0100

    Only publish verbose status updates to test channel

--- src/Slackbot.Net.Extensions.FplBot/GameweekLifecycle/Handlers/StatusUpdateHandler.cs
+++ src/Slackbot.Net.Extensions.FplBot/GameweekLifecycle/Handlers/StatusUpdateHandler.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Slackbot.Net.Extensions.FplBot.Abstractions;
 using Slackbot.Net.Extensions.FplBot.Helpers;
 using Slackbot.Net.Extensions.FplBot.Models;
@@ -10,18 +12,28 @@ namespace Slackbot.Net.Extensions.FplBot.GameweekLifecycle.Handlers
     {
         private readonly ISlackWorkSpacePublisher _publisher;
         private readonly ISlackTeamRepository _slackTeamRepo;
+        private readonly ILogger<StatusUpdateHandler> _logger;
 
-        public StatusUpdateHandler(ISlackWorkSpacePublisher publisher, ISlackTeamRepository slackTeamRepo)
+        public StatusUpdateHandler(ISlackWorkSpacePublisher publisher, ISlackTeamRepository slackTeamRepo, ILogger<StatusUpdateHandler> logger)
         {
             _publisher = publisher;
             _slackTeamRepo = slackTeamRepo;
+            _logger = logger;
         }
         
         public async Task OnStatusUpdates(IEnumerable<PlayerStatusUpdate> statusUpdates)
         {
             var slackTeam = await _slackTeamRepo.GetTeam("T0A9QSU83");
-            var formatted = Formatter.FormatStatusUpdates(statusUpdates);
-            await _publisher.PublishToWorkspace(slackTeam.TeamId, slackTeam.FplBotSlackChannel, formatted);
+            if (slackTeam.FplBotSlackChannel == "#fpltest")
+            {
+                var formatted = Formatter.FormatStatusUpdates(statusUpdates);
+                await _publisher.PublishToWorkspace(slackTeam.TeamId, slackTeam.FplBotSlackChannel, formatted);    
+            }
+            else
+            {
+                var formatted = Formatter.FormatStatusUpdates(statusUpdates);
+                _logger.LogInformation(formatted);
+            }
         }
     }
 }
