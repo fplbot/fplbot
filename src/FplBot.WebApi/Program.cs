9963725adc508ecc98dbdbbb8d5fcb651738294a
John Korsnes <johnkors@gmail.com>
2020-11-17 15:43:18 +0100

    Test JsonLines

--- src/FplBot.WebApi/Program.cs
+++ src/FplBot.WebApi/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using Serilog.Formatting.Json;
 using Serilog.Sinks.SystemConsole.Themes;
 
 namespace FplBot.WebApi
@@ -20,7 +21,8 @@ namespace FplBot.WebApi
                 .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
                     .ReadFrom.Configuration(hostingContext.Configuration)
                     .Enrich.FromLogContext()
-                    .WriteTo.Console(outputTemplate: "{Level:u3} {SourceContext} {Message:lj}{NewLine}{Exception}",theme: ConsoleTheme.None))
+                    .WriteTo.Console(new JsonFormatter())
+                )
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     var port = Environment.GetEnvironmentVariable("PORT") ?? "1337";
