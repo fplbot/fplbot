1fed053cd54756b3126a596a361c8fb232ce1251
John Korsnes <johnkors@gmail.com>
2020-11-19 14:08:29 +0100

    Adds explanation for property name

--- src/Slackbot.Net.Extensions.FplBot/ILoggerExtensions.cs
+++ src/Slackbot.Net.Extensions.FplBot/ILoggerExtensions.cs
@@ -6,9 +6,13 @@ namespace Microsoft.Extensions.Logging
 {
     internal static class ILoggerExtensions
     {
+        // Re-using the same as the correlationId ASP.NET Middleware, but re-defining it here to avoid dependency & it has a private accessor anyways :/ 
+        // https://github.com/ekmsystems/serilog-enrichers-correlation-id/blob/master/src/Serilog.Enrichers.CorrelationId/Enrichers/CorrelationIdEnricher.cs#L15
+        private const string CorrelationId = "CorrelationId";
+
         public static IDisposable BeginCorrelationScope(this ILogger logger)
         {
-            return logger.BeginScope(new Dictionary<string, object> {["CorrelationId"] = Guid.NewGuid()});
+            return logger.BeginScope(new Dictionary<string, object> {[CorrelationId] = Guid.NewGuid()});
         }
     }
 }
