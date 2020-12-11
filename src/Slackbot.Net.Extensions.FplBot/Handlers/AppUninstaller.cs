3e665c8ad71750769114eded1442a1ea01bf3d9c
John Korsnes <johnkors@gmail.com>
2020-12-11 10:34:07 +0100

    Adds notifications on app_uninstalls (#153)

--- src/Slackbot.Net.Extensions.FplBot/Handlers/AppUninstaller.cs
+++ src/Slackbot.Net.Extensions.FplBot/Handlers/AppUninstaller.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using FplBot.Messaging.Contracts.Events.v1;
+using NServiceBus;
 using Slackbot.Net.Endpoints.Abstractions;
 using Slackbot.Net.Extensions.FplBot.Abstractions;
 
@@ -7,15 +9,19 @@ namespace Slackbot.Net.Extensions.FplBot.Handlers
     public class AppUninstaller : IUninstall
     {
         private readonly ISlackTeamRepository _slackTeamRepo;
+        private readonly IMessageSession _messageSession;
 
-        public AppUninstaller(ISlackTeamRepository slackTeamRepo)
+        public AppUninstaller(ISlackTeamRepository slackTeamRepo, IMessageSession messageSession)
         {
             _slackTeamRepo = slackTeamRepo;
+            _messageSession = messageSession;
         }
         
         public async Task Uninstall(string teamId)
         {
+            var team = await _slackTeamRepo.GetTeam(teamId);
             await _slackTeamRepo.DeleteByTeamId(teamId);
+            await _messageSession.Publish(new AppUninstalled(team.TeamId, team.TeamName, (int)team.FplbotLeagueId, team.FplBotSlackChannel));
         }
     }
 }
