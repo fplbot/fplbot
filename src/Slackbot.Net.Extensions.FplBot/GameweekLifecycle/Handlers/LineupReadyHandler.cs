34754e738e4819c1c62870c28f47ba60c724fbaf
John Korsnes <johnkors@gmail.com>
2020-12-17 21:04:36 +0100

    Puts formatted lineups into slack threads (#161)

--- src/Slackbot.Net.Extensions.FplBot/GameweekLifecycle/Handlers/LineupReadyHandler.cs
+++ src/Slackbot.Net.Extensions.FplBot/GameweekLifecycle/Handlers/LineupReadyHandler.cs
@@ -1,21 +1,24 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Slackbot.Net.Extensions.FplBot.Abstractions;
 using Slackbot.Net.Extensions.FplBot.Helpers;
 using Slackbot.Net.Extensions.FplBot.Models;
+using Slackbot.Net.SlackClients.Http;
+using Slackbot.Net.SlackClients.Http.Models.Requests.ChatPostMessage;
 
 namespace Slackbot.Net.Extensions.FplBot.GameweekLifecycle.Handlers
 {
     public class LineupReadyHandler
     {
         private readonly ISlackTeamRepository _slackTeamRepo;
-        private readonly ISlackWorkSpacePublisher _publisher;
+        private readonly ISlackClientBuilder _builder;
         private readonly ILogger<LineupReadyHandler> _logger;
 
-        public LineupReadyHandler(ISlackTeamRepository slackTeamRepo, ISlackWorkSpacePublisher publisher, ILogger<LineupReadyHandler> logger)
+        public LineupReadyHandler(ISlackTeamRepository slackTeamRepo, ISlackClientBuilder builder, ILogger<LineupReadyHandler> logger)
         {
             _slackTeamRepo = slackTeamRepo;
-            _publisher = publisher;
+            _builder = builder;
             _logger = logger;
         }
         
@@ -23,10 +26,31 @@ namespace Slackbot.Net.Extensions.FplBot.GameweekLifecycle.Handlers
         {
             _logger.LogInformation("Handling new lineups");
             var slackTeams = await _slackTeamRepo.GetAllTeams();
+            var firstMessage = $"*Lineups {lineups.HomeTeamNameAbbr}-{lineups.AwayTeamNameAbbr} ready* 👇";
+            var formattedLineup = Formatter.FormatLineup(lineups);
             foreach (var slackTeam in slackTeams)
             {
-                var formatted = Formatter.FormatLineup(lineups);
-                await _publisher.PublishToWorkspace(slackTeam.TeamId, slackTeam.FplBotSlackChannel, formatted);
+                await PublishLineupToTeam(slackTeam);
+            }
+            
+            async Task PublishLineupToTeam(SlackTeam team)
+            {
+                var slackClient = _builder.Build(team.AccessToken);
+                try
+                {
+                    var res = await slackClient.ChatPostMessage(team.FplBotSlackChannel, firstMessage);
+                    if (res.Ok)
+                    {
+                        await slackClient.ChatPostMessage(new ChatPostMessageRequest
+                        {
+                            Channel = team.FplBotSlackChannel, thread_ts = res.ts, Text = formattedLineup, unfurl_links = "false"
+                        });
+                    }
+                }
+                catch (Exception e)
+                {
+                    _logger.LogWarning(e, e.Message);
+                }
             }
         }
     }
