601b757f0b476364e29dac422931bd0cb5190c44
Lars Skjelbek <lars.skjelbek@gmail.com>
2021-01-24 11:11:36 +0100

    Add some indexing info to admin view

--- src/FplBot.WebApi/Pages/Admin/Index.cshtml.cs
+++ src/FplBot.WebApi/Pages/Admin/Index.cshtml.cs
@@ -1,19 +1,24 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Fpl.Search.Indexing;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Nest;
 using Slackbot.Net.Extensions.FplBot.Abstractions;
 
 namespace FplBot.WebApi.Pages.Admin
 {
     public class Index : PageModel
     {
-        
         private readonly ISlackTeamRepository _teamRepo;
-        
+        private readonly IIndexBookmarkProvider _indexBookmarkProvider;
 
-        public Index(ISlackTeamRepository teamRepo)
+
+        public Index(
+            ISlackTeamRepository teamRepo,
+            IIndexBookmarkProvider indexBookmarkProvider)
         {
             _teamRepo = teamRepo;
+            _indexBookmarkProvider = indexBookmarkProvider;
             Workspaces = new List<SlackTeam>();
         }
         
@@ -24,8 +29,11 @@ namespace FplBot.WebApi.Pages.Admin
             {
                 Workspaces.Add(t);
             }
+
+            CurrentLeagueIndexingBookmark = await _indexBookmarkProvider.GetBookmark();
         }
 
         public List<SlackTeam> Workspaces { get; set; }
+        public int CurrentLeagueIndexingBookmark { get; set; }
     }
 }
