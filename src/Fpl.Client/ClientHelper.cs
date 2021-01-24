6c1dcec58f69f5aace51c3d3d34173238fc739c8
Lars Skjelbek <lars.skjelbek@gmail.com>
2021-01-24 23:06:32 +0100

    Increase the retry count when hitting 429s

--- src/Fpl.Client/ClientHelper.cs
+++ src/Fpl.Client/ClientHelper.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 
 namespace Fpl.Client
 {
     public static class ClientHelper
     {
-        public static async Task<T[]> PolledRequests<T>(Func<Task<T>[]> requests, ILogger logger, int retries = 3)
+        public static async Task<T[]> PolledRequests<T>(Func<Task<T>[]> requests, ILogger logger, int retries = 5)
         {
             var j = 0;
             while (j < retries)
