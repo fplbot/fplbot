a6d8287afc7689085bbf10b5af0b79777e5b95f3
Lars Skjelbek <lars.skjelbek@gmail.com>
2021-01-24 11:28:53 +0100

    When searching we now boost the home country of the admin of the league the slack channel is following

--- src/Slackbot.Net.Extensions.FplBot/Handlers/FplSearchHandler.cs
+++ src/Slackbot.Net.Extensions.FplBot/Handlers/FplSearchHandler.cs
@@ -18,18 +18,27 @@ namespace Slackbot.Net.Extensions.FplBot.Handlers
         private readonly ISearchClient _searchClient;
         private readonly IGameweekClient _gameweekClient;
         private readonly ISlackWorkSpacePublisher _workSpacePublisher;
+        private readonly ISlackTeamRepository _slackTeamRepo;
+        private readonly ILeagueClient _leagueClient;
+        private readonly IEntryClient _entryClient;
         private readonly ILogger<FplSearchHandler> _logger;
 
 
         public FplSearchHandler(
             ISearchClient searchClient,
             IGameweekClient gameweekClient,
-            ISlackWorkSpacePublisher workSpacePublisher, 
+            ISlackWorkSpacePublisher workSpacePublisher,
+            ISlackTeamRepository slackTeamRepo,
+            ILeagueClient leagueClient,
+            IEntryClient entryClient,
             ILogger<FplSearchHandler> logger)
         {
             _searchClient = searchClient;
             _gameweekClient = gameweekClient;
             _workSpacePublisher = workSpacePublisher;
+            _slackTeamRepo = slackTeamRepo;
+            _leagueClient = leagueClient;
+            _entryClient = entryClient;
             _logger = logger;
         }
 
@@ -39,8 +48,10 @@ namespace Slackbot.Net.Extensions.FplBot.Handlers
         {
             var term = ParseArguments(message);
 
+            string countryToBoost = await GetCountryToBoost(eventMetadata);
+
             var entriesTask = _searchClient.SearchForEntry(term, 10);
-            var leaguesTask = _searchClient.SearchForLeague(term, 10);
+            var leaguesTask = _searchClient.SearchForLeague(term, 10, countryToBoost);
 
             var entries = await entriesTask;
             var leagues = await leaguesTask;
@@ -85,6 +96,32 @@ namespace Slackbot.Net.Extensions.FplBot.Handlers
             await _workSpacePublisher.PublishToWorkspace(eventMetadata.Team_Id, message.Channel, sb.ToString());
             
             return new EventHandledResponse(sb.ToString());
+        }
+
+        private async Task<string> GetCountryToBoost(EventMetaData eventMetadata)
+        {
+            string countryToBoost = null;
+            if (eventMetadata.Team_Id != null)
+            {
+                var team = await _slackTeamRepo.GetTeam(eventMetadata.Team_Id);
+
+                if (team?.FplbotLeagueId != null)
+                {
+                    var league = await _leagueClient.GetClassicLeague((int) team.FplbotLeagueId);
+                    var adminEntry = league?.Properties?.AdminEntry;
+
+                    if (adminEntry != null)
+                    {
+                        var admin = await _entryClient.Get(adminEntry.Value);
+                        if (admin != null)
+                        {
+                            countryToBoost = admin.PlayerRegionShortIso;
+                        }
+                    }
+                }
+            }
+
+            return countryToBoost;
         }
 
         public override (string, string) GetHelpDescription() => ($"{CommandsFormatted} {{name}}", $"(:wrench: Beta) Search for teams or leagues. E.g. \"{CommandsFormatted} magnus carlsen\".");
