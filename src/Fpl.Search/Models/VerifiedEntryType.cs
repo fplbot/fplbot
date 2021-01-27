4d23c73aa4108409225417c4a2130b17e7ac99e4
Lars Skjelbek <lars.skjelbek@gmail.com>
2021-01-27 11:55:06 +0100

    Make sure that the VerifiedEntryType enum values can shuffle without breaking index mapping

--- src/Fpl.Search/Models/VerifiedEntryType.cs
+++ src/Fpl.Search/Models/VerifiedEntryType.cs
@@ -2,13 +2,13 @@
 {
     public enum VerifiedEntryType
     {
-        FootballerInPL,
-        Footballer,
-        ChessMaster,
-        Podcaster,
-        CommunityFame,
-        Actor,
-        TvFace,
-        Athlete
+        FootballerInPL = 0,
+        Footballer = 1,
+        ChessMaster = 2,
+        Podcaster = 3,
+        CommunityFame = 4,
+        Actor = 5,
+        TvFace = 6,
+        Athlete = 7
     }
 }
