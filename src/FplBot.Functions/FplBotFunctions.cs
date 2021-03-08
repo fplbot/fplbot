b57d134f89e7eead923a66095fcb04e5bd2f42d6
John Korsnes <johnkors@gmail.com>
2021-03-08 13:01:19 +0100

    Adds a HTTP endpoint in the Azure Function

--- src/FplBot.Functions/FplBotFunctions.cs
+++ src/FplBot.Functions/FplBotFunctions.cs
@@ -1,6 +1,8 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using NServiceBus;
 
@@ -13,6 +15,15 @@ namespace FplBot.Functions
         public FplBotFunctions(IFunctionEndpoint endpoint)
         {
             _endpoint = endpoint;
+        }
+
+        [FunctionName("Pong")]
+        public static string Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            string name = req.Query["name"];
+
+            return $"Hello, {name}";
         }
 
         [FunctionName("FplBotHandlers")]
