747979315b357896b127283b61b1b0c35eabf332
John Korsnes <johnkors@gmail.com>
2021-08-23 17:33:09 +0200

    Change suggestions slack team output

--- src/FplBot.WebApi/Handlers/Sagas/ThrottleSuggestionsSaga.cs
+++ src/FplBot.WebApi/Handlers/Sagas/ThrottleSuggestionsSaga.cs
@@ -15,21 +15,21 @@ namespace FplBot.WebApi.Handlers.Commands
     {
         public const int ThrottleTimeoutInSeconds = 60;
         public const string SlackChannel = "#fplbot-notifications";
-        public const string TeamId = "T0A9QSU83";
+        public const string TeamId = "T016B9N3U7P";
     }
-    
+
     public record VerifiedEntrySuggestionReceived(int EntryId, string Description) : IEvent;
     public record VerifiedPLEntrySuggestionReceived(int EntryId,string Description, int PlayerId) : IEvent;
-    
+
     public record SuggestionsThrottleTimeout();
 
-    public class ThrottleEntrySuggestionsSaga : Saga<AcccumulatedSuggestionsSagaData>, 
+    public class ThrottleEntrySuggestionsSaga : Saga<AcccumulatedSuggestionsSagaData>,
         IAmStartedByMessages<VerifiedEntrySuggestionReceived>,
         IHandleTimeouts<SuggestionsThrottleTimeout>
     {
         protected override void ConfigureHowToFindSaga(SagaPropertyMapper<AcccumulatedSuggestionsSagaData> mapper)
         {
-            mapper.ConfigureMapping<VerifiedEntrySuggestionReceived>(message => message.EntryId).ToSaga(sagaData => sagaData.EntryId);        
+            mapper.ConfigureMapping<VerifiedEntrySuggestionReceived>(message => message.EntryId).ToSaga(sagaData => sagaData.EntryId);
         }
 
         public async Task Handle(VerifiedEntrySuggestionReceived message, IMessageHandlerContext context)
@@ -46,23 +46,23 @@ namespace FplBot.WebApi.Handlers.Commands
             MarkAsComplete();
         }
     }
-    
-    public class ThrottlePlSuggestionsSaga : Saga<AcccumulatedPLSuggestionsSagaData>, 
-        IAmStartedByMessages<VerifiedPLEntrySuggestionReceived>, 
+
+    public class ThrottlePlSuggestionsSaga : Saga<AcccumulatedPLSuggestionsSagaData>,
+        IAmStartedByMessages<VerifiedPLEntrySuggestionReceived>,
         IHandleTimeouts<SuggestionsThrottleTimeout>
     {
         protected override void ConfigureHowToFindSaga(SagaPropertyMapper<AcccumulatedPLSuggestionsSagaData> mapper)
         {
-            mapper.ConfigureMapping<VerifiedPLEntrySuggestionReceived>(message => message.EntryId).ToSaga(sagaData => sagaData.EntryId);        
+            mapper.ConfigureMapping<VerifiedPLEntrySuggestionReceived>(message => message.EntryId).ToSaga(sagaData => sagaData.EntryId);
         }
-        
+
         public async Task Handle(VerifiedPLEntrySuggestionReceived message, IMessageHandlerContext context)
         {
             Data.SuggestionCount++;
             Data.PlayerId = message.PlayerId;
             if(!Data.Descriptions.Contains(message.Description))
                 Data.Descriptions.Add(message.Description);
-            
+
             await RequestTimeout(context, TimeSpan.FromSeconds(ThrottleSuggestionConstants.ThrottleTimeoutInSeconds), new SuggestionsThrottleTimeout());
         }
 
@@ -77,17 +77,17 @@ namespace FplBot.WebApi.Handlers.Commands
     {
         public int PlayerId { get; set; }
     }
-    
+
     public class AcccumulatedSuggestionsSagaData : ContainSagaData
     {
         public AcccumulatedSuggestionsSagaData()
         {
             Descriptions = new List<string>();
         }
-        
+
         public int EntryId { get; set; }
         public int SuggestionCount { get; set; }
-        
+
         public List<string> Descriptions { get; set; }
     }
 
@@ -142,7 +142,7 @@ namespace FplBot.WebApi.Handlers.Commands
             {
                 text = $"{message.EntryId} suggested{Counting(message.SuggestionCount)}, but it does not exist. 🤷‍♂️";
             }
-                  
+
             await context.SendLocal(new PublishToSlack(ThrottleSuggestionConstants.TeamId, ThrottleSuggestionConstants.SlackChannel, "Verified suggestion: " + text));
 
         }
