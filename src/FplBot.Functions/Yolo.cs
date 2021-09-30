ef80f76954b9ac259603b1d1b056bf63aab35fdf
John Korsnes <johnkors@gmail.com>
2021-09-30 21:48:12 +0200

    Adds Yolo-trigger

---
+++ src/FplBot.Functions/Yolo.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace FplBot.Functions
+{
+    public static class Yolo
+    {
+        [Function("Yolo")]
+        public static HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("Yolo");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+            
+        }
+    }
+}
