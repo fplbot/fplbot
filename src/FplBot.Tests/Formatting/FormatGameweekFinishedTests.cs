8eb9589d85ec4bc6c62878f3f512fe108e4b77b7
Lars Skjelbek <lars.skjelbek@gmail.com>
2021-11-05 13:39:07 +0100

    Update tests

--- src/FplBot.Tests/Formatting/FormatGameweekFinishedTests.cs
+++ src/FplBot.Tests/Formatting/FormatGameweekFinishedTests.cs
@@ -8,13 +8,13 @@ namespace FplBot.Tests.Formatting
     public class FormatGameweekFinishedTests
     {
         [Theory]
-        [InlineData(30, 10, 11, 11, "Gameweek 1 is finished. It was probably a disappointing one, with a global average of *30* points. I'm afraid your league did worse than this, with your *11* points average.")]
-        [InlineData(50, 10, 11, 11, "Gameweek 1 is finished. The global average was *50* points. I'm afraid your league did worse than this, with your *11* points average.")]
-        [InlineData(90, 10, 11, 11, "Gameweek 1 is finished. Must've been pretty intense, with a global average of *90* points. I'm afraid your league did worse than this, with your *11* points average.")]
-        [InlineData(30, 50, 40, 45, "Gameweek 1 is finished. It was probably a disappointing one, with a global average of *30* points. Your league did better than this, though - with *45* points average.")]
-        [InlineData(50, 50, 40, 45, "Gameweek 1 is finished. The global average was *50* points. I'm afraid your league did slightly worse than this, with your *45* points average.")]
-        [InlineData(90, 90, 91, 91, "Gameweek 1 is finished. Must've been pretty intense, with a global average of *90* points. Your league did slightly better than this, though - with *91* points average.")]
-        [InlineData(90, 90, 90, 91, "Gameweek 1 is finished. Must've been pretty intense, with a global average of *90* points. I guess your league is pretty mediocre, since you got the exact same *90* points average.")]
+        [InlineData(30, 10, 11, 11, "Gameweek 1 is finished. It was probably a disappointing one, with a global average of *30* points. Your league's average was *11* points.")]
+        [InlineData(50, 10, 11, 11, "Gameweek 1 is finished. The global average was *50* points. Your league's average was *11* points.")]
+        [InlineData(90, 10, 11, 11, "Gameweek 1 is finished. Must've been pretty intense, with a global average of *90* points. Your league's average was *11* points.")]
+        [InlineData(30, 50, 40, 45, "Gameweek 1 is finished. It was probably a disappointing one, with a global average of *30* points. Your league's average was *45* points.")]
+        [InlineData(50, 50, 40, 45, "Gameweek 1 is finished. The global average was *50* points. Your league's average was *45* points.")]
+        [InlineData(90, 90, 91, 91, "Gameweek 1 is finished. Must've been pretty intense, with a global average of *90* points. Your league's average was *91* points.")]
+        [InlineData(90, 90, 90, 91, "Gameweek 1 is finished. Must've been pretty intense, with a global average of *90* points. Your league's average was *90* points.")]
         public void FormatGameweekFinished_ShoudReturnCorrectMessage(
             int globalAverage, int entryScore1, int entryScore2, int entryScore3, string expectedMessage)
         {
