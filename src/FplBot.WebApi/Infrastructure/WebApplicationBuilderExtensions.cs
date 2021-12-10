e991494feec604bcdd3cbd56e705a9028a7d59ba
John Korsnes <johnkors@gmail.com>
2021-12-10 14:04:05 +0100

    Moves indexing to sep worker (#297)
    +semver:minor

--- src/FplBot.WebApi/Infrastructure/WebApplicationBuilderExtensions.cs
+++ src/FplBot.WebApi/Infrastructure/WebApplicationBuilderExtensions.cs
@@ -94,7 +94,6 @@ public static class WebApplicationBuilderExtensions
         services.AddMediatR(typeof(WebApplicationBuilderExtensions));
         if (!env.IsDevelopment())
         {
-            services.AddRecurringIndexer(configuration, conn);
         }
         else
         {
