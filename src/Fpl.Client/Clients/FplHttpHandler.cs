89e25d9474b71cb57c6fb770c342f953e016f6f8
John Korsnes <johnkors@gmail.com>
2022-02-24 15:08:32 +0100

    Remove FPL auth (#307)

--- src/Fpl.Client/Clients/FplHttpHandler.cs
+++ src/Fpl.Client/Clients/FplHttpHandler.cs
@@ -2,29 +2,6 @@ using System.Net;
 
 namespace Fpl.Client.Clients;
 
-public class FplHttpHandler : HttpClientHandler
-{
-    private readonly CookieFetcher _cookieFetcher;
-
-    public FplHttpHandler(CookieFetcher cookieFetcher)
-    {
-        _cookieFetcher = cookieFetcher;
-          
-            
-        AutomaticDecompression = DecompressionMethods.GZip;
-        SslProtocols = System.Security.Authentication.SslProtocols.Tls12;   
-    }
-
-    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-    {
-        var sessionCookie = await _cookieFetcher.GetSessionCookie();
-        request.Headers.Add("Cookie", sessionCookie);
-
-
-        return await base.SendAsync(request, cancellationToken);
-    }
-}
-    
 public class FplDelegatingHandler : DelegatingHandler
 {
     private readonly CookieFetcher _cookieFetcher;
