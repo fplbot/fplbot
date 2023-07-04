fb10753414aee8df0b907593ef988fed07d4db32
John Korsnes <johnkors@gmail.com>
2023-07-04 15:22:53 +0200

    Adds redis hack to non-API components

--- src/Fpl.EventPublishers.Console/HerokuRedisConfigParser.cs
+++ src/Fpl.EventPublishers.Console/HerokuRedisConfigParser.cs
@@ -1,3 +1,4 @@
+using System.Net.Security;
 using StackExchange.Redis;
 
 namespace Fpl.EventPublishers.Console;
@@ -11,7 +12,13 @@ public static class HerokuRedisConfigParser
         {
             ClientName = GetRedisUsername(uri),
             Password = GetRedisPassword(uri),
-            EndPoints = { GetRedisServerHostAndPort(redisUri)}
+            EndPoints = { GetRedisServerHostAndPort(redisUri)},
+            Ssl = true,
+            SslClientAuthenticationOptions = s => new SslClientAuthenticationOptions
+            {
+                TargetHost = GetHost(redisUri),
+                RemoteCertificateValidationCallback = (h, a, c, k) => true,
+            }
         };
     }
 
@@ -20,4 +27,6 @@ public static class HerokuRedisConfigParser
     private static string GetRedisUsername(Uri redisUri) => redisUri.UserInfo.Split(":")[0];
 
     private static string GetRedisServerHostAndPort(string redisUri) => redisUri.Split("@")[1];
+
+    private static string GetHost(string redisUri) => redisUri.Split(":")[0];
 }
