9a63b86d03e3c24e57702314a8187104039c79fc
John Korsnes <johnkors@gmail.com>
2024-02-07 09:31:11 +0100

    Bugfix: txt instead of evt

--- src/FplBot.WebApi/Handlers/Events/AppInstalledHandler.cs
+++ src/FplBot.WebApi/Handlers/Events/AppInstalledHandler.cs
@@ -12,36 +12,36 @@ public class AppInstalledHandler(IGuildRepository guildRepo,
     IConfiguration config,
     ILogger<AppInstalledHandler> logger) : IHandleMessages<AppInstalled>
 {
-    public async Task Handle(AppInstalled message, IMessageHandlerContext context)
+    public async Task Handle(AppInstalled @event, IMessageHandlerContext context)
     {
-        var count = message.Platform switch
+        var count = @event.Platform switch
         {
             ChatPlatform.Discord => (await guildRepo.GetAllGuilds()).Count(),
             ChatPlatform.Slack => (await slackRepo.GetAllTeams()).Count(),
             _ => -1
         };
 
-        var msg = count switch
+        var text = count switch
         {
-            _ when count % 1000 == 0 => $"??🎉🎉🎉🏁✅ 🎂 {count} {message.Platform} installs! ‼️ 👀",
-            _ when count % 100 == 0 => $"💯{count} {message.Platform} installs!",
-            _ when count % 10 == 0 => $"{count} {message.Platform} installs!",
+            _ when count % 1000 == 0 => $"??🎉🎉🎉🏁✅ 🎂 {count} {@event.Platform} installs! ‼️ 👀",
+            _ when count % 100 == 0 => $"💯{count} {@event.Platform} installs!",
+            _ when count % 10 == 0 => $"{count} {@event.Platform} installs!",
             _ => null
         };
 
-        if (msg is not null)
+        if (text is not null)
         {
-            logger.LogInformation("Sending count msg. {Count} {Platform} installs", count, message.Platform);
+            logger.LogInformation("Sending count msg. {Count} {Platform} installs", count, @event.Platform);
 
             var token = config.GetValue<string>("SlackToken_FplBot_Workspace");
             var env = config.GetValue<string>("DOTNET_ENVIRONMENT");
             var prefix = env == "Production" ? "" : $"{env}: ";
             var client = builder.Build(token);
-            await client.ChatPostMessage("#fplbot-notifications", $"{prefix}{message}");
+            await client.ChatPostMessage("#fplbot-notifications", $"{prefix}{text}");
         }
         else
         {
-            logger.LogInformation("No message sent for {Platform} install. Count is {Count}", message.Platform, count);
+            logger.LogInformation("No message sent for {Platform} install. Count is {Count}", @event.Platform, count);
         }
     }
 }
