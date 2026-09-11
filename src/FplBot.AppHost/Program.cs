e943b17e21b6bcc2b5f45699f512b197ab1024ee
John Korsnes <johnkors@gmail.com>
2026-09-11 17:01:27 +0200

    bump almost servicebus and change ports for dashboard and asb-emulator (#391)

--- src/FplBot.AppHost/Program.cs
+++ src/FplBot.AppHost/Program.cs
@@ -4,7 +4,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var redis = builder.AddRedis("redis", port: 6379)
     .WithArgs("--requirepass", "devpassword");
-builder.AddServiceBusEmulator("servicebus");
+builder.AddServiceBusEmulator("servicebus", dashboardPort:20000, port: 6000);
 var elasticsearch = builder.AddElasticsearch("elasticsearch",
         password: builder.AddParameter("elasticsearch-password", "dev", secret: true), port: 9200);
 
